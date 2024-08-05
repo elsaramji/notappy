6579a3dca11440775164d8176543e1cefaeeaae2
elsaramji <elsaramegymahmoud404@gmail.com>
2024-08-05 22:21:57 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,13 @@
+// models/note_model.dart
+
+class NoteModel {
+  String title;
+  String content;
+  String data;
+  int color;
+  NoteModel(
+      {required this.title,
+      required this.content,
+      required this.data,
+      required this.color});
+}
